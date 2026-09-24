bece2637710e69103af7ee61cd0e7f458bc801ed
kovardhini <kovardhini06@gmail.com>
2026-09-24 13:59:16 +0530

    Configure app for Render deployment

--- app.py
+++ app.py
@@ -120,4 +120,5 @@ with gr.Blocks(title="Credit Card Fraud Detector", theme=gr.themes.Soft()) as de
         outputs=[amount, time] + v_sliders,
     )
 
-demo.launch()
+import os
+demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))git add app.py
